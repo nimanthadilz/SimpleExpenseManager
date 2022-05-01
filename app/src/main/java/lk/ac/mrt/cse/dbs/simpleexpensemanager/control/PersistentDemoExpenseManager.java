01a1a4ca350bdd8b225f2c7848c37ace872baa9a
Nimantha Cooray <nimanthacooray@gmail.com>
2022-05-01 12:43:27 +0530

    Implement PersistentDemoExpenseManager

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentDemoExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentDemoExpenseManager.java
@@ -1,20 +1,37 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
+import android.content.Context;
+
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.DatabaseHelper;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.ui.MainActivity;
 
 public class PersistentDemoExpenseManager extends ExpenseManager {
-    public PersistentDemoExpenseManager() { setup(); }
+    private Context context;
+    public PersistentDemoExpenseManager(Context context) {
+        this.context = context;
+        setup();
+    }
 
     @Override
     public void setup() {
-        AccountDAO persistentAccountDAO = new PersistentAccountDAO();
+        DatabaseHelper databaseHelper = new DatabaseHelper(this.context);
+
+        AccountDAO persistentAccountDAO = new PersistentAccountDAO(databaseHelper);
         setAccountsDAO(persistentAccountDAO);
 
-        TransactionDAO persistentTransactionDAO = new PersistentTransactionDAO();
+        TransactionDAO persistentTransactionDAO = new PersistentTransactionDAO(databaseHelper);
         setTransactionsDAO(persistentTransactionDAO);
+
+        // dummy data
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
     }
 }
